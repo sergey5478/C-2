83f07d7eae7c92829a6c7f108f7d13ee10ff8186
sergey <sergey.fomin.90@inbox.ru>
2022-08-08 21:46:11 +0400

    add_task9

---
+++ task9/Program.cs
@@ -0,0 +1,19 @@
+﻿int GetRandomNumberInRange(int minNumber, int maxNumber)
+{
+    int result = new Random().Next(minNumber, maxNumber + 1);
+    return result;
+}
+int GetMaxDigitFromNumber(int number)
+{
+    int result = number / 10;
+    int secondDigit = number % 10;
+    if (secondDigit > result)
+    {
+        result = secondDigit;
+    }
+    return result;
+}
+
+int randomNumber = GetRandomNumberInRange(10, 99);
+int maxDigit = GetMaxDigitFromNumber(randomNumber);
+Console.WriteLine($"Наибольшая цифра числа {randomNumber} является {maxDigit}");
