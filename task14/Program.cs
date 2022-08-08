262ff92acaa570966509c76013f67a2076df2a7c
sergey <sergey.fomin.90@inbox.ru>
2022-08-08 23:37:48 +0400

    add_task14

---
+++ task14/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход число
+ и проверяет, кратно ли оно одновременно 7 и 23.
+ 14 -> нет 
+46 -> нет 
+161 -> да
+ */
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int divisionOnSeven = number % 7;
+int divisionOn23 = number % 23;
+if(divisionOnSeven == 0 && divisionOn23 == 0)
+{
+    Console.WriteLine($"Число {number} делится на 7 и на 23 без остатка");
+}
+else
+{
+    Console.WriteLine($"Число {number} НЕ делится на 7 и на 23 без остатка");
+}
